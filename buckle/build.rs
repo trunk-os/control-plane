5347358bdb023674d1830618d908446c22ed3329
Erik Hollensbe <git@hollensbe.org>
2025-09-02 10:28:42 -0700

    fix dependencies and update to latest prost/tonic
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- buckle/build.rs
+++ buckle/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/buckle.proto")?;
+    tonic_prost_build::compile_protos("proto/buckle.proto")?;
 
     Ok(())
 }
