52b2f27b35d0f3aee760cbc9e75e6bc48f875db8
Erik Hollensbe <git@hollensbe.org>
2025-10-10 13:49:52 -0700

    one more shot at this
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- charon/src/package.rs
+++ charon/src/package.rs
@@ -357,17 +357,24 @@ impl CompiledPackage {
 
 		let _ = client.systemd().await?.stop_unit(unit_name.clone()).await;
 
+		let mut iters = 0;
+
 		'wait: loop {
 			match client.systemd().await?.unit_info(unit_name.clone()).await {
 				Ok(status) => match status.status.last_run_state {
 					LastRunState::Dead | LastRunState::Failed | LastRunState::Exited => {
-						tokio::time::sleep(std::time::Duration::from_millis(500)).await;
+						// if we had to try a few times, the process probably needs some time to stop behind
+						// the scenes.
+						if iters > 0 {
+							tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+						}
 						break 'wait;
 					}
 					_ => {}
 				},
 				Err(_) => break 'wait,
 			}
+			iters += 1;
 			tokio::time::sleep(std::time::Duration::from_millis(100)).await;
 		}
 
