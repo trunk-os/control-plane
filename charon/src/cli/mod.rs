6d6f6355c87eec9310452a6e2c53594773aadcda
Erik Hollensbe <git@hollensbe.org>
2025-10-10 20:15:10 -0700

    I am a fool
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- charon/src/cli/mod.rs
+++ charon/src/cli/mod.rs
@@ -252,6 +252,11 @@ pub fn generate_container_command(
 		cmd.append(&mut vec!["-p".into(), portmap]);
 	}
 
+	for (hostport, localport) in &package.networking.expose_ports {
+		let portmap = format!("{}:{}", hostport, localport);
+		cmd.append(&mut vec!["-p".into(), portmap]);
+	}
+
 	for volume in &package.storage.volumes {
 		// FIXME: create filesystems on block devices.
 		//        ignoring them for now
