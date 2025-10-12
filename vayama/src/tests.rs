46d3b8374353786477d057d7f1f48e84385f6246
Erik Hollensbe <git@hollensbe.org>
2025-10-12 13:11:19 -0700

    add some state in; we'll use this to verify the run logic
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- vayama/src/tests.rs
+++ vayama/src/tests.rs
@@ -1,4 +1,7 @@
 use crate::*;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+static STATE: AtomicUsize = AtomicUsize::new(0);
 
 // this is less effort than populating a Map etc
 fn get_migration(name: &str) -> Option<Migration> {
@@ -7,7 +10,10 @@ fn get_migration(name: &str) -> Option<Migration> {
 			name: name.to_string(),
 			dependencies: Default::default(),
 			check: None,
-			run: Box::new(|| Ok(())),
+			run: Box::new(|| {
+				STATE.store(STATE.load(Ordering::Acquire) + 1, Ordering::Release);
+				Ok(())
+			}),
 			post_check: None,
 		}),
 		"run_only_with_error" => Some(Migration {
@@ -21,42 +27,60 @@ fn get_migration(name: &str) -> Option<Migration> {
 			name: name.to_string(),
 			dependencies: Default::default(),
 			check: Some(Box::new(|| Ok(()))),
-			run: Box::new(|| Ok(())),
+			run: Box::new(|| {
+				STATE.store(STATE.load(Ordering::Acquire) + 1, Ordering::Release);
+				Ok(())
+			}),
 			post_check: None,
 		}),
 		"successful_run_with_successful_post_check" => Some(Migration {
 			name: name.to_string(),
 			dependencies: Default::default(),
 			check: None,
-			run: Box::new(|| Ok(())),
+			run: Box::new(|| {
+				STATE.store(STATE.load(Ordering::Acquire) + 1, Ordering::Release);
+				Ok(())
+			}),
 			post_check: Some(Box::new(|| Ok(()))),
 		}),
 		"successful_run_with_successful_both_checks" => Some(Migration {
 			name: name.to_string(),
 			dependencies: Default::default(),
 			check: Some(Box::new(|| Ok(()))),
-			run: Box::new(|| Ok(())),
+			run: Box::new(|| {
+				STATE.store(STATE.load(Ordering::Acquire) + 1, Ordering::Release);
+				Ok(())
+			}),
 			post_check: Some(Box::new(|| Ok(()))),
 		}),
 		"successful_run_with_failing_check" => Some(Migration {
 			name: name.to_string(),
 			dependencies: Default::default(),
 			check: Some(Box::new(|| Err(MigrationError::Unknown))),
-			run: Box::new(|| Ok(())),
+			run: Box::new(|| {
+				STATE.store(STATE.load(Ordering::Acquire) + 1, Ordering::Release);
+				Ok(())
+			}),
 			post_check: None,
 		}),
 		"successful_run_with_failing_post_check" => Some(Migration {
 			name: name.to_string(),
 			dependencies: Default::default(),
 			check: None,
-			run: Box::new(|| Ok(())),
+			run: Box::new(|| {
+				STATE.store(STATE.load(Ordering::Acquire) + 1, Ordering::Release);
+				Ok(())
+			}),
 			post_check: Some(Box::new(|| Err(MigrationError::Unknown))),
 		}),
 		"successful_run_with_failing_both_checks" => Some(Migration {
 			name: name.to_string(),
 			dependencies: Default::default(),
 			check: Some(Box::new(|| Err(MigrationError::Unknown))),
-			run: Box::new(|| Ok(())),
+			run: Box::new(|| {
+				STATE.store(STATE.load(Ordering::Acquire) + 1, Ordering::Release);
+				Ok(())
+			}),
 			post_check: Some(Box::new(|| Err(MigrationError::Unknown))),
 		}),
 		_ => None,
@@ -71,6 +95,8 @@ async fn execute_migration(name: &str) -> Result<(), MigrationError> {
 async fn execute_migration_with_state(
 	name: &str, state: MigrationState,
 ) -> Result<(), MigrationError> {
+	STATE.store(0, Ordering::Release);
+
 	get_migration(name)
 		.expect(&format!("test migration ({}) missing from table", name))
 		.execute(&state)
