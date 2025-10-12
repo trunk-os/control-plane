4b1dbf39de7dfeb5da565367bed5761b3bd2eb34
Erik Hollensbe <git@hollensbe.org>
2025-10-12 12:44:35 -0700

    some tests
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- vayama/src/lib.rs
+++ vayama/src/lib.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 use std::{collections::BTreeMap, path::PathBuf, process::ExitStatus};
 
 use anyhow::{Result, anyhow};
---
+++ vayama/src/tests.rs
@@ -0,0 +1,54 @@
+use crate::*;
+
+// this is less effort than populating a Map etc
+fn get_migration(name: &str) -> Option<Migration> {
+	match name {
+		"successful_run" => Some(Migration {
+			name: name.to_string(),
+			dependencies: Default::default(),
+			check: None,
+			run: Box::new(|| Ok(())),
+			post_check: None,
+		}),
+		"run_only_with_error" => Some(Migration {
+			name: name.to_string(),
+			dependencies: Default::default(),
+			check: None,
+			run: Box::new(|| Err(MigrationError::Unknown)),
+			post_check: None,
+		}),
+		_ => None,
+	}
+}
+
+async fn execute_migration(name: &str) -> Result<(), MigrationError> {
+	let state: MigrationState = Default::default();
+	execute_migration_with_state(name, state).await
+}
+
+async fn execute_migration_with_state(
+	name: &str, state: MigrationState,
+) -> Result<(), MigrationError> {
+	get_migration(name)
+		.expect(&format!("test migration ({}) missing from table", name))
+		.execute(&state)
+		.await
+}
+
+#[tokio::test]
+async fn basic_run() {
+	assert!(execute_migration("successful_run").await.is_ok());
+	assert!(execute_migration("run_only_with_error").await.is_err());
+}
+
+#[test]
+#[ignore]
+fn run_with_checks() {}
+
+#[test]
+#[ignore]
+fn run_with_dependencies() {}
+
+#[test]
+#[ignore]
+fn run_with_recovery() {}
