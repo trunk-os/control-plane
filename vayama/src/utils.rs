ace41eeaa87d9203101d851311ad5692c2fa2491
Erik Hollensbe <git@hollensbe.org>
2025-10-12 20:58:41 -0700

    I really need a break
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- vayama/src/utils.rs
+++ vayama/src/utils.rs
@@ -13,7 +13,8 @@ pub async fn generic_command(command: &str, args: Vec<&str>) -> Result<String> {
 		Ok(String::from_utf8(output.stdout)?)
 	} else {
 		Err(anyhow!(
-			"podman command {:?} exited with status {:?}: stderr: [{}]",
+			"command `{}` [args: {:?}] exited with status {:?}: stderr: [{}]",
+			command,
 			args,
 			output.status.code(),
 			String::from_utf8(output.stderr)?
