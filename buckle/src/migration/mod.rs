8696b4a9fed3ae88120d5c592b9876796a62a0d7
Erik Hollensbe <git@hollensbe.org>
2025-10-21 01:53:29 -0700

    gut are gory, but this should work pretty well
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- buckle/src/migration/mod.rs
+++ buckle/src/migration/mod.rs
@@ -1,13 +1,17 @@
-#![allow(dead_code)]
-use std::{collections::HashMap, pin::Pin};
+#![allow(dead_code, unused_variables, unused_mut)]
+use std::{collections::HashMap, pin::Pin, sync::Arc};
 use thiserror::Error;
+use tokio::sync::Mutex;
 
 mod plans;
 
 pub type MigrationState = HashMap<String, String>;
 pub type MigrationResult = Result<MigrationState, MigrationError>;
-pub type MigrationFunc =
-	Box<dyn Fn(MigrationState) -> Pin<Box<dyn Future<Output = MigrationResult>>>>;
+pub type MigrationFunc = Arc<
+	Mutex<
+		Box<dyn Send + Fn(MigrationState) -> Pin<Box<dyn Send + Future<Output = MigrationResult>>>>,
+	>,
+>;
 
 #[derive(Debug, Clone, Default, Error)]
 pub enum MigrationError {
@@ -18,19 +22,21 @@ pub enum MigrationError {
 	UnknownWithMessage(String),
 }
 
-pub struct Migration(Vec<MigrationFunc>);
+pub struct Migration<'a>(Vec<&'a dyn BoxedMigrationClosure>);
 
-impl Migration {
+impl<'a> Migration<'a> {
 	pub async fn run(&self, mut state: MigrationState) -> Result<(), MigrationError> {
 		for func in &self.0 {
-			state = (func)(state).await?;
+			let closure = func.closure().await;
+			let lock = closure.lock().await;
+			state = (*lock)(state).await?;
 		}
 
 		Ok(())
 	}
 }
 
-pub async fn run_migrations(migrations: Vec<Migration>) -> anyhow::Result<()> {
+pub async fn run_migrations(migrations: Vec<Migration<'_>>) -> anyhow::Result<()> {
 	for migration in &migrations {
 		migration.run(Default::default()).await?;
 	}
@@ -38,31 +44,56 @@ pub async fn run_migrations(migrations: Vec<Migration>) -> anyhow::Result<()> {
 	Ok(())
 }
 
-mod tests {
-	#![allow(unused_variables)]
-	use super::*;
+#[async_trait::async_trait]
+pub trait BoxedMigrationClosure {
+	async fn call_with_state(&self, state: MigrationState) -> MigrationResult;
+	async fn closure(&self) -> MigrationFunc;
+}
 
-	macro_rules! make_migration_func {
-		($name:ident, $state:ident, $func:block) => {
-			fn $name(
-				state: &mut MigrationState,
-			) -> Pin<Box<impl Future<Output = MigrationResult>>> {
-				let $state = state.clone();
-				Box::pin(async move { ($func) })
-			}
-		};
+#[derive(Clone)]
+pub struct MigrationClosure(MigrationFunc);
+
+#[async_trait::async_trait]
+impl BoxedMigrationClosure for MigrationClosure {
+	async fn call_with_state(&self, state: MigrationState) -> MigrationResult {
+		let lock = self.0.lock().await;
+		(*lock)(state).await
 	}
 
-	macro_rules! build_migration_set {
-    ($(($name:ident, $state:ident, $func:block)),*) => {{
-    $(
-    make_migration_func!($name, $state, $func);
-    )*
+	async fn closure(&self) -> MigrationFunc {
+		self.0.clone()
+	}
+}
+
+macro_rules! make_migration_func {
+	($name:ident, $state:ident, $func:block) => {
+		MigrationClosure(Arc::new(Mutex::new(Box::new(
+			|mut state: MigrationState| {
+				let $state = state.clone();
+				Box::pin(async move { $func })
+			},
+		))))
+	};
+}
+
+macro_rules! build_migration_set {
+    ($state:ident, $(($name:ident, $func:block)),*) => {{
+      let mut v = Vec::new();
+      $(
+      {
+        let func = make_migration_func!($name, $state, $func);
+        v.push(func);
+      }
+      )*
+      v
     }}
   }
 
-	async fn migration_test() {
+mod tests {
+	use super::*;
+
+	async fn migration_success() {
 		let state: MigrationState = Default::default();
-		build_migration_set!((fart, state, { Ok(state) }));
+		build_migration_set!(state, (foo, { Ok(state) }), (bar, { Ok(state) }));
 	}
 }
