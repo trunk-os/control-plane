d1c99154cc45a6594cc2628a4b8b804467345ac0
Erik Hollensbe <git@hollensbe.org>
2025-10-21 14:48:50 -0700

    sketching out some ideas for plans
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

---
+++ buckle/src/migration/plans.rs
@@ -0,0 +1,35 @@
+use super::*;
+use crate::{build_migration_set, make_migration_func};
+
+fn prometheus() -> Migration {
+	let state = MigrationState::default();
+	build_migration_set!(
+		state,
+		(check_prometheus, { Ok(state) }),
+		(install_prometheus, { Ok(state) }),
+		(configure_prometheus, { Ok(state) }),
+		(restart_prometheus, { Ok(state) })
+	)
+}
+
+fn grafana() -> Migration {
+	let state = MigrationState::default();
+	build_migration_set!(
+		state,
+		(check_grafana, { Ok(state) }),
+		(install_grafana, { Ok(state) }),
+		(configure_grafana, { Ok(state) }),
+		(restart_grafana, { Ok(state) })
+	)
+}
+
+fn node_exporter() -> Migration {
+	let state = MigrationState::default();
+	build_migration_set!(
+		state,
+		(check_node_exporter, { Ok(state) }),
+		(install_node_exporter, { Ok(state) }),
+		(configure_node_exporter, { Ok(state) }),
+		(restart_node_exporter, { Ok(state) })
+	)
+}
